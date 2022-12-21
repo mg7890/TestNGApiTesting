6c0ca940ed02781217abae6f1091bd30e2da8fc7
mg7890 <mutlu.gunduz@yahoo.com>
2022-12-20 19:36:20 -0500

    minor changes

--- src/test/java/petStoreApiTests/PetStoreApiTests.java
+++ src/test/java/petStoreApiTests/PetStoreApiTests.java
@@ -2,11 +2,15 @@ package petStoreApiTests;
 
 
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.io.File;
 
+import org.hamcrest.Matchers;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import groovyjarjarantlr4.v4.runtime.atn.SemanticContext.AND;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -14,11 +18,19 @@ import io.restassured.response.Response;
 public class PetStoreApiTests {
 	
 	
+	int catID, petID, donkeyID;
+	
+	@BeforeTest
+	public void setup() {
+		RestAssured.baseURI="https://petstore.swagger.io/v2";
+	}
+	
+	
 	@Test (dependsOnMethods = "postAPet")
 	public void getPetById() {
 		RestAssured
 		.given().accept(ContentType.JSON)
-		.when().get("https://petstore.swagger.io/v2/pet/78901234")
+		.when().get("/pet/78901234")
 		.then().statusCode(200);
 	}
 	
@@ -26,15 +38,16 @@ public class PetStoreApiTests {
 	@Test
 	public void findPetByStatus() {
 		RestAssured.given().accept(ContentType.JSON).contentType("application/json").param("string", "pending").when()
-				.get("https://petstore.swagger.io/v2/pet/findByStatus").then().statusCode(200)
+				.get("/pet/findByStatus").then().statusCode(200)
 				.contentType("application/json");
 	}
 	
-	@Test (dependsOnMethods = "postACat")
+	
+	@Test (dependsOnMethods = {"postACat", "updateCat"})
 	public void getById() {
 
 		Response myResponse = RestAssured.given().accept(ContentType.JSON).when()
-				.get("https://petstore.swagger.io/v2/pet/232323");
+				.get("/pet/232323");
 		myResponse.prettyPrint();
 		// Verifying the status code
 		myResponse.then().assertThat().statusCode(200).and().contentType("application/json");
@@ -59,6 +72,9 @@ public class PetStoreApiTests {
 		String name2 = myResponse.jsonPath().get("category.name");
 		System.out.println("Second name: " + name2);
 		Assert.assertEquals(name2, "cat");
+		
+		String catStatus = myResponse.body().jsonPath().get("status");
+		Assert.assertEquals(catStatus, "pending");
 	}
 	
 	
@@ -85,12 +101,82 @@ public class PetStoreApiTests {
 				+ "}";
 		
 		Response myResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json").body(catRequestBody).when()
-				.post("https://petstore.swagger.io/v2/pet");
+				.post("/pet");
 		
-		myResponse.then().statusCode(200).and().contentType("application/json");	
+		myResponse.then().statusCode(200).and().contentType("application/json");		
+		catID = myResponse.jsonPath().get("id");
+	}
+	
+	
+	
+	
+	//update the cat status to pending
+	@Test  (dependsOnMethods = "postACat")
+	public void updateCat() {
+		String updateRequestBody="{\r\n"
+				+ "  \"id\": 789012345,\r\n"
+				+ "  \"category\": {\r\n"
+				+ "    \"id\": 555,\r\n"
+				+ "    \"name\": \"feline\"\r\n"
+				+ "  },\r\n"
+				+ "  \"name\": \"kitty\",\r\n"
+				+ "  \"photoUrls\": [\r\n"
+				+ "    \"string\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"tags\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"id\": 0,\r\n"
+				+ "      \"name\": \"CAT\"\r\n"
+				+ "    }\r\n"
+				+ "  ],\r\n"
+				+ "  \"status\": \"pending\"\r\n"
+				+ "}";
+		
+		Response catUpdateResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json")
+		.body(updateRequestBody).when().put("/pet");
+		
+		catUpdateResponse.then().statusCode(200).and().contentType("application/json");
+		Assert.assertEquals(catUpdateResponse.body().jsonPath().get("status"),"pending");	
+		
+	}
+	
+	
+	
+	
+	
+	
+	//Attempt to update the cat status using invalid ID
+	@Test 
+	public void invalidUpdateCat() {
+		String updateRequestBody="{\r\n"
+				+ "  \"id\": '789012345',\r\n"
+				+ "  \"category\": {\r\n"
+				+ "    \"id\": 555,\r\n"
+				+ "    \"name\": \"feline\"\r\n"
+				+ "  },\r\n"
+				+ "  \"name\": \"kitty\",\r\n"
+				+ "  \"photoUrls\": [\r\n"
+				+ "    \"string\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"tags\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"id\": 0,\r\n"
+				+ "      \"name\": \"CAT\"\r\n"
+				+ "    }\r\n"
+				+ "  ],\r\n"
+				+ "  \"status\": \"pending\"\r\n"
+				+ "}";
+		
+		Response catUpdateResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json")
+		.body(updateRequestBody).when().put("/pet");
+				
+		catUpdateResponse.then().statusCode(400).and().contentType("application/json");
+		Assert.assertEquals(catUpdateResponse.body().jsonPath().get("message"),"bad input");	
 	}
 	
 
+	
+	
 	
 	@Test
 	public void postAPet() {
@@ -124,9 +210,105 @@ public class PetStoreApiTests {
 				+ "}";
 		
 		Response myResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json").body(dogRequestBody).when()
-				.post("https://petstore.swagger.io/v2/pet");
+				.post("/v2/pet");	
 		
-		myResponse.then().statusCode(200).and().contentType("application/json");
+		myResponse.then().statusCode(200).and().contentType("application/json");		
+		petID = myResponse.jsonPath().get("id");
+	}
+	
+	
+	
+	
+	//create a pet with request body in json file	
+	@Test
+	public void createDonkeyWithJsonFile() {
+		File donkeyRequestBodyFile = new File("./src/test/resources/jsonTestData/createPet.json");
+				
+		Response myResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json").body(donkeyRequestBodyFile).when()
+		.post("/pet");	
+		
+		myResponse.then().statusCode(200).and().contentType("application/json");		
+		donkeyID = myResponse.jsonPath().get("id");
+	}
+	
+	
+	
+	
+	//RestAssured chain validation
+	@Test
+	public void chainValidation() {
+		File requestBodyFile = new File("./src/test/resources/jsonTestData/createPet.json");
+		
+		Response myResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json").body(requestBodyFile).when()
+		.post("/pet");
+		
+		myResponse
+		.then().assertThat().statusCode(200)
+		.and().assertThat().contentType("application/json")
+		.and().assertThat().body("id", Matchers.equalTo(7890))
+		.and().assertThat().body("category.id", Matchers.equalTo(33))
+		.and().assertThat().body("category.name",Matchers.equalTo("donkey"))
+		.and().assertThat().body("name", Matchers.equalTo("Stubborn Donkey"))
+		.and().assertThat().body("tags[41].id",Matchers.equalTo(41))
+		.and().assertThat().body("tags[41].name",Matchers.equalTo("Donkey41"))
+		.and().assertThat().body("tags[42].id",Matchers.equalTo(42))
+		.and().assertThat().body("tags[42].name",Matchers.equalTo("Donkey42"))
+		.and().assertThat().body("tags[43].id",Matchers.equalTo(43))
+		.and().assertThat().body("tags[43].name",Matchers.equalTo("Donkey43"))
+		.and().assertThat().body("status",Matchers.equalTo("available"));
+		
+		myResponse.prettyPrint();
+		
+		donkeyID = myResponse.jsonPath().getInt("id");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@AfterTest 
+	public void cleanup() {
+		//deleteCat();
+		//deletePet();
+		//deleteDonkey();
+	}
+	
+	
+	public void deletePet() {	
+		Response deleteResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json")
+		.delete("/pet/"+petID);  //must provide full URL including the petID 	
+		deleteResponse.then().statusCode(200).and().contentType("application/json");
+		Assert.assertEquals(deleteResponse.body().jsonPath().get("message"), ""+petID);	
+	}
+	
+	
+	
+	public void deleteCat() {	
+		Response deleteResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json")
+		.delete("/pet/"+catID);  //must provide full URL including the petID 	
+		deleteResponse.then().statusCode(200).and().contentType("application/json");
+		Assert.assertEquals(deleteResponse.body().jsonPath().get("message"), ""+catID);	
+	}
+	
+	
+	public void deleteDonkey() {	
+		Response deleteResponse = RestAssured.given().accept(ContentType.JSON).contentType("application/json")
+		.delete("/pet/"+donkeyID);  //must provide full URL including the petID 	
+		deleteResponse.then().statusCode(200).and().contentType("application/json");
+		Assert.assertEquals(deleteResponse.body().jsonPath().get("message"), ""+donkeyID);	
 	}
 }
 
